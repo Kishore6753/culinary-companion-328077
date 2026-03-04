c443e82d867d0493d70fa7c6217b09d5003dc7aa
Kavia AI <production@kavia.ai>
2026-03-04 06:51:53 +0000

    Checkpoint 1 - cgaab48e2c

---
+++ .init/.linter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/culinary-companion-328077/recipe_frontend_app
+npm run lint
+ESLINT_EXIT_CODE=$?
+npm run build
+BUILD_EXIT_CODE=$?
+if [ $ESLINT_EXIT_CODE -ne 0 ] || [ $BUILD_EXIT_CODE -ne 0 ]; then
+   exit 1
+fi
+
